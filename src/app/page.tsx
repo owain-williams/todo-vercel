1b618122452829c0bd3552516863f8f18fbe28d0
Owain Williams <owain@andersdigital.com>
2024-05-04 14:07:09 +0100

    feat: user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import { TrashIcon } from "lucide-react";
-
-import { auth } from "@clerk/nextjs/server";
-import { db } from "@/lib/db";
-import TodoItem from "@/components/todo-item";
-import { Todo } from "@prisma/client";
 import TodoAddForm from "@/components/todo-add-form";
+import TodoItem from "@/components/todo-item";
+import { db } from "@/lib/db";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function HomePage() {
   const { userId } = auth();
@@ -21,14 +15,22 @@ export default async function HomePage() {
   });
 
   return (
-    <div className="flex flex-col items-center py-12">
-      <h1 className="text-4xl font-bold mb-8">Todo List</h1>
-      <div className="w-full max-w-md">
-        <TodoAddForm userId={userId} />
-        <div className="divide-y divide-gray-200 dark:divide-gray-800">
-          {todos.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
-          ))}
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center justify-between">
+        <div />
+        <div className="p-4">
+          <UserButton />
+        </div>
+      </div>
+      <div className="flex flex-col items-center py-2">
+        <h1 className="text-4xl font-bold mb-8">Todo List</h1>
+        <div className="w-full max-w-md">
+          <TodoAddForm userId={userId} />
+          <div className="divide-y divide-gray-200 dark:divide-gray-800">
+            {todos.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
